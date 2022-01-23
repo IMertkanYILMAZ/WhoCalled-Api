32f3f8b2f8b3dbb2269967b40a5a45fa91e29a5e
imyilmaz <iylmz13@gmail.com>
2022-01-23 23:55:18 +0300

    Comments deleted

--- src/main/java/dev/imy/kimaradi/services/CallService.java
+++ src/main/java/dev/imy/kimaradi/services/CallService.java
@@ -31,19 +31,11 @@ public class CallService {
         final List<Call> missedCallers = callDao.getMissedCallNumberByRecipientPhoneNumber(phoneNumber);
 
         if (missedCallers == null || missedCallers.isEmpty()){
-            //return MissedCallResponse.builder()
-            //        .message("There is no missed calls...")
-            //        .build();
+
             return "There is no missed calls...";
         }
 
         final List<MissedCall> missedCalls = missedCallers.stream().map(call -> MissedCall.builder().callerPhone(call.getCallerPhoneNumber()).callTime(call.getCallDate()).build()).collect(Collectors.toList());
-
-        final List<String> callerNumbersList = (List<String>) missedCalls.stream().map(s -> s.getCallerPhone()).collect(Collectors.toList());
-
-        Map<String, Long> callGroupByNumberList =
-                callerNumbersList.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
-        //Example : {5393154387=3, 5325137801=1}
 
         Map<String, List<LocalDateTime>> callGroupByCallTimeList = missedCalls.stream().collect(Collectors.groupingBy(
                 MissedCall::getCallerPhone,
@@ -58,12 +50,6 @@ public class CallService {
                     .max(LocalDateTime::compareTo).get() + " , ";
         }
         return "You have " + missedCalls.stream().count() + " missed calls \n" + messagePart2_service;
-
-        //return MissedCallResponse.builder()
-        //        .missedCallList(missedCalls)
-        //        .message_part2()
-        //        .message(Constants.messageEn)
-        //        .build();
     }
 
 }
